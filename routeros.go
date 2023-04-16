050ab72bdf1ea64fc849a13bc4a0da0d4917f345
Cedric BAIL <cedric@ddlm.me>
2023-04-15 22:19:30 -0600

    Add most of CAPsMAN and begining of Wireless and Interfaces.

--- routeros.go
+++ routeros.go
@@ -29,6 +29,93 @@ var routerOSCommands = map[string][]RouterOSView{
 			},
 		},
 		{
+			title: "Provisioning",
+			path: "/caps-man/provisioning",
+			headers: []RouterOSHeader{
+			},
+		},
+		{
+			title: "Configuration",
+			path: "/caps-man/configuration",
+			headers: []RouterOSHeader{
+				{"Name", "name"},
+				{"Mode", "mode"},
+				{"SSID", "ssid"},
+				{"TX Chains", "tx-chains"},
+				{"RX Chains", "rx-chains"},
+				{"Country", "country"},
+				{"Installation", "installation"},
+				{"Security", "security"},
+				{"Datapath", "datapath"},
+				{"Channel", "channel"},
+			},
+		},
+		{
+			title: "Channel",
+			path: "/caps-man/channel",
+			headers: []RouterOSHeader{
+				{"Name", "name"},
+				{"Frequency", "frequency"},
+				{"Control Channel Width", "control-channel-width"},
+				{"TX Power", "tx-power"},
+			},
+		},
+		{
+			title: "Datapath",
+			path: "/caps-man/datapath",
+			headers: []RouterOSHeader{
+				{"Name", "name"},
+				{"Client to Client forwarding", "client-to-client-forwarding"},
+				{"Bridge", "bridge"},
+				{"Local forwarding", "local-forwarding"},
+			},
+		},
+		{
+			title: "Security configuration",
+			path: "/caps-man/security",
+			headers: []RouterOSHeader{
+				{"Name", "name"},
+				{"Authentication Types", "authentication-types"},
+				{"Encryption", "encryption"},
+				{"Group Encryption", "group-encryption"},
+				{"Group Key Update", "group-key-update"},
+			},
+		},
+		{
+			title: "Access List",
+			path: "/caps-man/access-list",
+			headers: []RouterOSHeader{
+				{"Interface", "interface"},
+				{"Signal Range", "signal-range"},
+				{"Client To Client Forwarding", "client-to-client-forwarding"},
+			},
+		},
+		{
+			title: "Remote Cap",
+			path: "/caps-man/remote-cap",
+			headers: []RouterOSHeader{
+				{"Address", "address"},
+				{"Name", "name"},
+				{"Board", "board"},
+				{"Serial", "serial"},
+				{"Version", "version"},
+				{"Identity", "identity"},
+				{"Base Mac", "base-mac"},
+				{"State", "state"},
+				{"Radios", "radios"},
+			},
+		},
+		{
+			title: "Radio",
+			path: "/caps-man/radio",
+			headers: []RouterOSHeader{
+				{"Radio Max", "radio-mac"},
+				{"Remote Cap Name", "remote-cap-name"},
+				{"Remote Cap Identity", "remote-cap-identity"},
+				{"Interface", "interface"},
+			},
+		},
+		{
 			title: "Registration Table",
 			path:  "/caps-man/registration-table",
 			headers: []RouterOSHeader{
@@ -50,12 +137,30 @@ var routerOSCommands = map[string][]RouterOSView{
 		{
 			title: "Interface",
 			path:  "/interface/ethernet",
+			headers: []RouterOSHeader{
+				{"Name", "name"},
+				{"Actual MTU", "mtu"},
+				{"L2 MTU", "l2mtu"},
+				{"TX", "tx-bytes"},
+				{"RX", "rx-bytes"},
+			},
 		},
 	},
 	"Wireless": {
 		{
 			title: "WiFi Interfaces",
 			path:  "/interface/wireless",
+			headers: []RouterOSHeader{
+				{"Name", "name"},
+				{"Actual MTU", "mtu"},
+				{"MAC Address", "mac-address"},
+				{"ARP", "arp"},
+				{"Mode", "mode"},
+				{"Band", "band"},
+				{"Channel Width", "channel-width"},
+				{"Frequency", "frequency"},
+				{"SSID", "ssid"},
+			},
 		},
 	},
 	"DHCP Server": {
